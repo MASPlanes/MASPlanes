3f38be4ed9b392a0898bd931a6fa9ccb1341abe6
Marc Pujol <mpujol@iiia.csic.es>
2013-03-14 16:32:22 +0100

    Added the ability to bid for the shortest total path cost in omniscient ssi auctions

--- src/es/csic/iiia/planes/omniscient/SSIAllocation.java
+++ src/es/csic/iiia/planes/omniscient/SSIAllocation.java
@@ -56,6 +56,8 @@ import java.util.logging.Logger;
 public class SSIAllocation extends AbstractAllocationStrategy {
     private static final Logger LOG = Logger.getLogger(SSIAllocation.class.getName());
 
+    Map<OmniscientPlane, PathCost> assignments = new TreeMap<OmniscientPlane, PathCost>();
+
     @Override
     public void allocate(World w,
         OmniscientPlane[] planes,
@@ -67,10 +69,10 @@ public class SSIAllocation extends AbstractAllocationStrategy {
         LOG.finer("Tasks to allocate: " + pendingTasks);
 
         // Initialize the planes paths and first bids
-        Map<OmniscientPlane, List<Task>> assignments = new TreeMap<OmniscientPlane, List<Task>>();
+        assignments.clear();
         PriorityQueue<Bid> bids = new PriorityQueue<Bid>(planes.length);
         for (OmniscientPlane p : planes) {
-            assignments.put(p, new ArrayList<Task>());
+            assignments.put(p, new PathCost(new ArrayList<Task>(),0));
             Bid best = best(p, assignments.get(p), pendingTasks, visibilityMap.get(p));
             if (best != null) {
                 LOG.finer("New bid: " + best);
@@ -87,8 +89,9 @@ public class SSIAllocation extends AbstractAllocationStrategy {
             // we compute the plane's new bid without changing anything else
             if (pendingTasks.contains(best.task)) {
                 LOG.finer("Accepted bid: " + best);
-                List<Task> planeTasks = assignments.get(best.plane);
-                planeTasks.add(best.bp.index, best.task);
+                PathCost pcost = assignments.get(best.plane);
+                pcost.path.add(best.bp.index, best.task);
+                assignments.put(best.plane, new PathCost(pcost.path, best.bp.totalCost));
                 pendingTasks.remove(best.task);
             }
 
@@ -102,7 +105,7 @@ public class SSIAllocation extends AbstractAllocationStrategy {
         // Now set the assignments
         reverseMap.clear();
         for (OmniscientPlane p : planes) {
-            List<Task> path = assignments.get(p);
+            List<Task> path = assignments.get(p).path;
             if (!path.isEmpty()) {
                 assignmentMap.put(p, path.get(0));
                 reverseMap.put(path.get(0), p);
@@ -112,21 +115,21 @@ public class SSIAllocation extends AbstractAllocationStrategy {
         }
     }
 
-    private BestPosition bestPosition(OmniscientPlane p, List<Task> path, Task t) {
+    private BestPosition bestPosition(OmniscientPlane p, PathCost path, Task t) {
         final Location tl = t.getLocation();
 
         // Go first?
         double minCost = p.getLocation().distance(tl);
         int best = 0;
-        if (!path.isEmpty()) {
-            minCost += tl.distance(path.get(0).getLocation());
-            minCost -= p.getLocation().distance(path.get(0).getLocation());
+        if (!path.path.isEmpty()) {
+            minCost += tl.distance(path.path.get(0).getLocation());
+            minCost -= p.getLocation().distance(path.path.get(0).getLocation());
         }
 
         // Go after the i'th (which starts at 0)
-        for (int i=0; i<path.size()-1; i++) {
-            Location prev = path.get(i).getLocation();
-            Location next = path.get(i+1).getLocation();
+        for (int i=0; i<path.path.size()-1; i++) {
+            Location prev = path.path.get(i).getLocation();
+            Location next = path.path.get(i+1).getLocation();
             double cost = prev.distance(tl)
                         + tl.distance(next)
                         - prev.distance(next);
@@ -138,20 +141,31 @@ public class SSIAllocation extends AbstractAllocationStrategy {
         }
 
         // Go at the end
-        if (!path.isEmpty()) {
-            double cost = path.get(path.size()-1).getLocation().distance(tl);
+        if (!path.path.isEmpty()) {
+            double cost = path.path.get(path.path.size()-1).getLocation().distance(tl);
             if (cost < minCost) {
                 minCost = cost;
-                best = path.size();
+                best = path.path.size();
             }
         }
 
         LOG.log(Level.FINEST, "Best position for {0} in {1}: {2} ({3})", new Object[]{t, p, best, minCost});
         LOG.log(Level.FINEST, "Current path: {0}", path);
-        return new BestPosition(best, minCost);
+        return new BestPosition(best, minCost, path.cost + minCost);
     }
 
-    private Bid best(OmniscientPlane p, List<Task> currentPath, List<Task> remaining, Set<Task> visibles) {
+    @Override
+    public List<Location> getPlannedLocations(OmniscientPlane plane) {
+        List<Location> locations = new ArrayList<Location>();
+        for (Task t : assignments.get(plane).path) {
+            locations.add(t.getLocation());
+        }
+        return locations;
+    }
+
+
+
+    private Bid best(OmniscientPlane p, PathCost currentPath, List<Task> remaining, Set<Task> visibles) {
         Task best = null;
         BestPosition bestBp = null;
         for (Task t : remaining) {
@@ -161,7 +175,7 @@ public class SSIAllocation extends AbstractAllocationStrategy {
             }
 
             BestPosition bp = bestPosition(p, currentPath, t);
-            if (bestBp == null || bp.cost < bestBp.cost) {
+            if (bestBp == null || bp.additionalCost < bestBp.additionalCost) {
                 best = t;
                 bestBp = bp;
             }
@@ -175,17 +189,28 @@ public class SSIAllocation extends AbstractAllocationStrategy {
         return result;
     }
 
+    class PathCost {
+        public final List<Task> path;
+        public final double cost;
+        public PathCost(List<Task> path, double cost) {
+            this.path = path;
+            this.cost = cost;
+        }
+    }
+
     class BestPosition {
         public final int index;
-        public final Double cost;
-        public BestPosition(int index, double cost) {
+        public final Double additionalCost;
+        public final Double totalCost;
+        public BestPosition(int index, double additionalCost, double totalCost) {
             this.index = index;
-            this.cost = cost;
+            this.additionalCost = additionalCost;
+            this.totalCost = totalCost;
         }
 
         @Override
         public String toString() {
-            return "BestPosition{" + "index=" + index + ", cost=" + cost + '}';
+            return "BestPosition{" + "index=" + index + ", cost=" + additionalCost + '}';
         }
     }
 
@@ -202,7 +227,7 @@ public class SSIAllocation extends AbstractAllocationStrategy {
 
         @Override
         public int compareTo(Bid t) {
-            return bp.cost.compareTo(t.bp.cost);
+            return bp.totalCost.compareTo(t.bp.totalCost);
         }
 
         @Override
