30ddf63b3ec090c8b01fc9270b764b8c05a713ea
Marc Pujol-Gonzalez <mpujol@iiia.csic.es>
2014-05-07 20:58:35 +0200

    Tutorial Step 6: Determining winners and reallocating

--- src/main/java/es/csic/iiia/planes/tutorial/PSIAuctionsBehavior.java
+++ src/main/java/es/csic/iiia/planes/tutorial/PSIAuctionsBehavior.java
@@ -69,6 +69,11 @@ public class PSIAuctionsBehavior extends AbstractBehavior<TutorialPlane> {
         if (getAgent().getWorld().getTime() % 4 == 0) {
             openAuctions();
         }
+
+        // Compute auction winners only if we have received bids in this step
+        if (!collectedBids.isEmpty()) {
+            computeAuctionWinners();
+        }
     }
 
     private void openAuctions() {
@@ -77,6 +82,47 @@ public class PSIAuctionsBehavior extends AbstractBehavior<TutorialPlane> {
             OpenAuctionMessage msg = new OpenAuctionMessage(t);
             plane.send(msg);
         }
+    }
+
+    private void computeAuctionWinners() {
+        // For each auction we opened
+        for (Task t : collectedBids.keySet()) {
+            // Determine the winner
+            BidMessage winner = computeAuctionWinner(collectedBids.get(t));
+            // Reallocate the task
+            reallocateTask(winner);
+        }
+    }
+
+    private BidMessage computeAuctionWinner(List<BidMessage> bids) {
+        BidMessage winner = null;
+        double minCost = Double.MAX_VALUE;
+
+        for (BidMessage bid : bids) {
+            if (bid.getCost() < minCost) {
+                winner = bid;
+                minCost = bid.getCost();
+            }
+        }
+
+        return winner;
+    }
+
+    private void reallocateTask(BidMessage winner) {
+        TutorialPlane plane = getAgent();
+
+        // No need to reallocate when the task is already ours
+        if (winner.getSender() == plane) {
+            return;
+        }
+
+        // Remove the task from our list of pending tasks
+        plane.removeTask(winner.getTask());
+
+        // Send it to the auction's winner
+        ReallocateMessage msg = new ReallocateMessage(winner.getTask());
+        msg.setRecipient(winner.getSender());
+        plane.send(msg);
     }
 
     public void on(OpenAuctionMessage auction) {
